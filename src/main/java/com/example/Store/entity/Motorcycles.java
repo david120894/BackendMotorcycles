371f0695d0ce5694a827f0fc92037462847d2bbf
david <jesus251293@gmail.com>
2024-05-20 18:35:37 -0500

    uploading project

---
+++ src/main/java/com/example/Store/entity/Motorcycles.java
@@ -0,0 +1,24 @@
+package com.example.Store.entity;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="cycles")
+public class Motorcycles {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long motorCyclesId;
+
+    private String brand;
+
+    private String model;
+
+    private String year;
+
+    private Double price;
+}
